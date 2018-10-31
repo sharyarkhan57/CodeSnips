2ed072e9212b0a1890c93a50a99682c3e80f2ead
Sharyar Khan <sharyarkhan57@gmail.com>
2018-10-31 18:17:26 -0400

    First Commit
    Iterative solution to an inorder tree traversal

---
+++ inorderTraversal.java
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+public static List<Integer> inorderTraversal(TreeNode node){
+
+		List<Integer> solution = new LinkedList<Integer>();
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+
+		TreeNode curr = node;
+
+		while(!stack.isEmpty() || curr != null){
+
+			// go as left as possible
+			if(curr !=null){
+				stack.add(curr);
+				curr = curr.left;
+			}else{
+				// go up,
+				curr = stack.peek();
+				solution.add(stack.pop().val);
+				// go as far right as possible
+				curr = curr.right;
+			}
+		}
+
+	return solution;
+}
